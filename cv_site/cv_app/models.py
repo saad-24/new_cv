946b3c53198d9ba2004e0e6d999f138e0b450cc0
saad-24 <designitivity@gmail.com>
2023-08-24 04:46:19 +0500

    updated models

--- cv_site/cv_app/models.py
+++ cv_site/cv_app/models.py
@@ -5,7 +5,7 @@ from django.contrib.auth.models import User
 class Company(models.Model):
     name = models.CharField(max_length=200, null=False, default='ABC Solutions')
     designation = models.TextField(max_length=100, null=False, default='Engineer')
-    start_duration = models.CharField(max_length=100,null=False, default='1')
+    start_duration = models.DateField(null=False, default='2000-12-31')
     end_duration = models.CharField(max_length=100, null=False, default='Present')
     job_description = models.CharField(max_length=5000, null=False, default='Backend Engineer')
     user = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
@@ -21,4 +21,22 @@ class user_data(models.Model):
 
 class Education(models.Model):
     institute = models.CharField(max_length=500, null=False, default='ABC Institute')
+    degree = models.CharField(max_length=400, null=False , default="Bachelor's")
+    start_duration = models.IntegerField(null=False, default=2000)
+    end_duration = models.CharField(max_length=100, null=False, default='Present')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+
+
+class Certifications(models.Model):
+    name = models.CharField(max_length=2000, null=False,default='Certificate Name')
+    issuer = models.CharField(max_length=600,  null=False, default='Issuer Name')
+    issueDate = models.DateField(null=False, default='2000-12-31')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+
+class Skills(models.Model):
+    name = models.CharField(max_length=800, default='Python')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default='1')
+
+
+
 
