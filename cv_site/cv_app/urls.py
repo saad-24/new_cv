694a9fd6c93ec405afaba657f442fae6c36aab09
saad-24 <designitivity@gmail.com>
2023-09-01 02:49:11 +0500

    updated app urls

--- cv_site/cv_app/urls.py
+++ cv_site/cv_app/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path('logout/', views.logout_view, name='logout'),
     path('input_text/', views.input_text, name='input_text'),
     path('add_company/', views.add_company, name='add_company'),
+    path('add_certification/', views.add_certification, name='add_certification'),
+    path('add_education/', views.add_education, name='add_education'),
+    path('add_skills/', views.add_skill, name='add_skills'),
 ]
